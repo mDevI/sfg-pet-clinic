561a51cbf28ebeef173214cae01645d600c5e208
Sergei Belonosov <2belonosov@gmail.com>
2019-09-28 21:34:12 +0700

    added Vet list and upgraded controller . closes #15

--- pet-clinic-web/src/main/java/com/mdevi/sfgpetclinic/controllers/VetController.java
+++ pet-clinic-web/src/main/java/com/mdevi/sfgpetclinic/controllers/VetController.java
@@ -1,15 +1,23 @@
 package com.mdevi.sfgpetclinic.controllers;
 
+import com.mdevi.sfgpetclinic.services.VetService;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
 @Controller
 public class VetController {
 
-    @RequestMapping(method = RequestMethod.GET, value = {"/vets", "/vets/index", "/vets/index.html"})
-    public String listVets() {
+    private final VetService vetService;
 
+    public VetController(VetService vetService) {
+        this.vetService = vetService;
+    }
+
+    @RequestMapping(method = RequestMethod.GET, value = {"/vets", "/vets/index", "/vets/index.html"})
+    public String listVets(Model model) {
+        model.addAttribute("vets", vetService.findAll());
         return "vets/index";
     }
 }
