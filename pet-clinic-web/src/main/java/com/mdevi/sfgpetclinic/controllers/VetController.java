16f7d84a98cf4a85cfd734273b77b2471ec71cdc
Sergei Belonosov <2belonosov@gmail.com>
2019-10-02 09:31:18 +0300

    broken links are fixed. Closes #45.

--- pet-clinic-web/src/main/java/com/mdevi/sfgpetclinic/controllers/VetController.java
+++ pet-clinic-web/src/main/java/com/mdevi/sfgpetclinic/controllers/VetController.java
@@ -15,9 +15,10 @@ public class VetController {
         this.vetService = vetService;
     }
 
-    @RequestMapping(method = RequestMethod.GET, value = {"/vets", "/vets/index", "/vets/index.html"})
+    @RequestMapping(method = RequestMethod.GET, value = {"/vets", "/vets/index", "/vets/index.html", "/vets.html"})
     public String listVets(Model model) {
         model.addAttribute("vets", vetService.findAll());
         return "vets/index";
     }
+
 }
