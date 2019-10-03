6c96c1f8ff18337f51e850e95fbd6a4c5ef6ecf5
Sergei Belonosov <2belonosov@gmail.com>
2019-10-03 12:43:04 +0300

    Updated owner class with JPA annotations. Closes #36.

--- pet-clinic-data/src/main/java/com/mdevi/sfgpetclinic/model/Pet.java
+++ pet-clinic-data/src/main/java/com/mdevi/sfgpetclinic/model/Pet.java
@@ -1,12 +1,21 @@
 package com.mdevi.sfgpetclinic.model;
 
+import javax.persistence.*;
 import java.time.LocalDate;
 
+@Entity
+@Table(name = "pets")
 public class Pet extends BaseEntity {
 
+    @Column(name = "name")
     private String name;
+    @Column(name = "birth_date")
     private LocalDate birthDate;
+    @ManyToOne
+    @JoinColumn(name = "pet_id")
     private PetType petType;
+    @ManyToOne
+    @JoinColumn(name = "owner_id")
     private Owner owner;
 
     public String getName() {
