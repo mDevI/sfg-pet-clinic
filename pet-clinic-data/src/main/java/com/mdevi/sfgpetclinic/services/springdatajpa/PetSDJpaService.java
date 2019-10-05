590f1eff100837acbb7551bd84a528b094ad6145
Sergei Belonosov <2belonosov@gmail.com>
2019-10-05 20:39:52 +0700

    added Pet jpa service. closes #43

---
+++ pet-clinic-data/src/main/java/com/mdevi/sfgpetclinic/services/springdatajpa/PetSDJpaService.java
@@ -0,0 +1,48 @@
+package com.mdevi.sfgpetclinic.services.springdatajpa;
+
+import com.mdevi.sfgpetclinic.model.Pet;
+import com.mdevi.sfgpetclinic.repositories.PetRepository;
+import com.mdevi.sfgpetclinic.services.PetService;
+import org.springframework.context.annotation.Profile;
+import org.springframework.stereotype.Service;
+
+import java.util.HashSet;
+import java.util.Set;
+
+@Service
+@Profile("springdatajpa")
+public class PetSDJpaService implements PetService {
+
+    private final PetRepository petRepository;
+
+    public PetSDJpaService(PetRepository petRepository) {
+        this.petRepository = petRepository;
+    }
+
+    @Override
+    public Pet findById(Long id) {
+        return petRepository.findById(id).orElse(null);
+    }
+
+    @Override
+    public Pet save(Pet pet) {
+        return petRepository.save(pet);
+    }
+
+    @Override
+    public Set<Pet> findAll() {
+        Set<Pet> pets = new HashSet<>(0);
+        petRepository.findAll().forEach(pets::add);
+        return pets;
+    }
+
+    @Override
+    public void delete(Pet pet) {
+        petRepository.delete(pet);
+    }
+
+    @Override
+    public void deleteById(Long id) {
+        petRepository.deleteById(id);
+    }
+}
