d4e05c345e416db83ce61c530b5c1fd15c53d467
Sergei Belonosov <2belonosov@gmail.com>
2019-10-10 06:58:09 +0300

    added MockMvc tests for Owner controller . Closes #52.

--- pet-clinic-web/src/test/java/com/mdevi/sfgpetclinic/SfgPetClinicApplicationTest.java
+++ pet-clinic-web/src/test/java/com/mdevi/sfgpetclinic/SfgPetClinicApplicationTest.java
@@ -5,7 +5,7 @@ import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
 
 @ExtendWith(SpringExtension.class)
-public class SfgPetClinicApplicationTests {
+public class SfgPetClinicApplicationTest {
 
     @Test
     public void contextLoads() {
