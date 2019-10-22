31a986208e94622f57d9d13e90007a6a452ce419
Sergei Belonosov <2belonosov@gmail.com>
2019-10-22 13:42:45 +0300

    display Owner record. closes #53

--- pet-clinic-web/src/main/java/com/mdevi/sfgpetclinic/controllers/OwnerController.java
+++ pet-clinic-web/src/main/java/com/mdevi/sfgpetclinic/controllers/OwnerController.java
@@ -3,7 +3,10 @@ package com.mdevi.sfgpetclinic.controllers;
 import com.mdevi.sfgpetclinic.services.OwnerService;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
 
 @Controller
 @RequestMapping("/owners")
@@ -24,5 +27,18 @@ public class OwnerController {
     @RequestMapping("/find")
     public String findOwner() {
         return "notimplemented";
+    }
+
+    /**
+     * Custom handler for displaying an owner.
+     *
+     * @param ownerId the ID of the owner to display
+     * @return a ModelMap with the model attributes for the view
+     */
+    @GetMapping("/{ownerId}")
+    public ModelAndView showOwner(@PathVariable("ownerId") String ownerId) {
+        ModelAndView modelAndView = new ModelAndView("/owners/ownerDetails");
+        modelAndView.addObject(this.ownerService.findById(Long.valueOf(ownerId)));
+        return modelAndView;
     }
 }
